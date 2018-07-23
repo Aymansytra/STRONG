e502a9adf365613dac296ac77f534932fc102671
Aymansytra <40816752+Aymansytra@users.noreply.github.com>
2018-07-23 06:08:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,30 +32,45 @@ client.on('message', message => {
     if (message.content === 'سيطره') {
         message.reply('عيووووووونه');
       }
+});
     
     client.on('message', message => {
     if (message.content === 'الشعار') {
         message.reply('** Ӄᴎ∂⍣㦵 | الشعار في آقاريو   || ! KND , | آلشعار في الديسكورد **');
       }
+});
         
         client.on('message', message => {
     if (message.content === 'السلام عليك') {
         message.reply('**وعڵيڪم ٱڵسڵٱم ورحمهہ ٱڵڵهہ وبرڪٱتهہ**');
       }
+});
     
         client.on('message', message => {
     if (message.content === 'باك') {
         message.reply('**ﯟڸــگــمۘ نۨــﯣڕټ ڀــٰا̍ڠــٰٱ̍ڸــﮯ**');
       }
-            
+});
+           
             client.on('message', message => {
     if (message.content === 'برب') {
         message.reply('**ٺــﯧْــٺ , ﷲ مــ؏ــک**');
+      }  
+});
+          
+          client.on('message', message => {
+    if (message.content === 'zg') {
+        message.reply('pong');
       }
+});
+
             
 client.on('message', message => {
     if (message.content === 'التاق') {
         message.reply('** Ӄᴎ∂⍣㦵 | الشعار في آقاريو   || ! KND , | آلشعار في الديسكورد **');
+      }
+});
 
-      
+
+
 client.login(process.env.BOT_TOKEN);
