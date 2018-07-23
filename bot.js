0d335e402637ae290b08f3421744244295f260b5
Aymansytra <40816752+Aymansytra@users.noreply.github.com>
2018-07-23 05:46:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "^";
+const prefix = '.'
+
 client.on('ready', () => {
-client.user.setGame(' KND GAMES  ','https://www.twitch.tv/sytra_ayman');
-    console.log('I am ready!');
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
 
 client.on('message', message => {
@@ -34,6 +56,5 @@ client.on('message', message => {
 client.on('message', message => {
     if (message.content === 'التاق') {
         message.reply('** Ӄᴎ∂⍣㦵 | الشعار في آقاريو   || ! KND , | آلشعار في الديسكورد **');
-      }
-          });
+ 
 client.login(process.env.BOT_TOKEN);
