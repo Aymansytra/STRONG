a39a78c05ddd406c882ee1461d61666c578788d8
Aymansytra <40816752+Aymansytra@users.noreply.github.com>
2018-08-05 13:38:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -95,9 +95,8 @@ welcomer.sendFile(canvas.toBuffer())
 client.on("guildMemberAdd", (member) => {
 let channel = member.guild.channels.get("475233806677442560");
 client.guild.channels.get('475233806677442560').send(`welcome To ${member.guild.name}\nتم دعوته بواسطة  ${Invite.inviter} `) ;       
-})
-dat[Inv] = Invite.uses;
-});
-});
-});
+      })
+      })
+      });
+  });
 client.login(process.env.BOT_TOKEN);
